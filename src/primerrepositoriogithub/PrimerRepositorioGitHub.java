7226605953b90c4c5b5214fdf51389a6b2e28bc4
Owner <Owner@Owner-PC>
2018-09-08 14:37:29 -0300

    Probando primer repositorio en GitHub

---
+++ src/primerrepositoriogithub/PrimerRepositorioGitHub.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package primerrepositoriogithub;
+
+/**
+ *
+ * @author Owner
+ */
+public class PrimerRepositorioGitHub {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Aprendiendo a subir mi primer repositorio en GitHub
+        System.out.println("Hola profe... ¡Y hola, GitHub!");
+    }
+    
+}
